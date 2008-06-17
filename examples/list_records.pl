979ff9a19a78877527ef48a1fdd2a2a49c06f64b
Michael Greb <michael@thegrebs.com>
2008-06-17 16:36:28 +0000

    Import of MIKEGRB/WebService-Linode-0.01 from CPAN.
    gitpan-cpan-distribution: WebService-Linode
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MIKEGRB/WebService-Linode-0.01.tar.gz
    gitpan-cpan-author:       MIKEGRB
    gitpan-cpan-maturity:     released

---
+++ examples/list_records.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use WebService::Linode::DNS;
+
+my $api = new WebService::Linode::DNS (
+	apikey => '',
+	debug => 5,
+	fatal => 1,
+	nocache => 0
+);
+
+for my $domain (@{$api->domainList}) {
+	print "$domain->{domain} $domain->{type}\n";
+	print "Records:\n";
+	
+	my $rrs = $api->domainResourceList( domainid => $domain->{domainid} );
+	for my $rr ( @$rrs ) {
+		printf("\t%-10s %5s %-20s\n",
+			 $rr->{name}, $rr->{type}, $rr->{target}
+		);
+	}	
+}
