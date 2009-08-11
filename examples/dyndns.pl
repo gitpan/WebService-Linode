0e7c5248c6b420a5ed6d14bf98f576a497f857a3
Michael Greb <michael@thegrebs.com>
2009-08-11 02:01:56 +0000

    Import of MIKEGRB/WebService-Linode-0.04 from CPAN.
    gitpan-cpan-distribution: WebService-Linode
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MIKEGRB/WebService-Linode-0.04.tar.gz
    gitpan-cpan-author:       MIKEGRB
    gitpan-cpan-maturity:     released

--- examples/dyndns.pl
+++ examples/dyndns.pl
@@ -3,32 +3,37 @@
 use strict;
 use warnings;
 
-use WebService::Linode::DNS;
+use WebService::Linode;
 use LWP::Simple;
 
 # yourname.com is a master zone with a resource record of type A named home
 # that should point to home IP.
 
-my $apikey = '';
+my $apikey = 'your api key';
 my $domain = 'yourname.com';
 my $record = 'home';
-my $ipfile = '/home/username/.lastip';	# file to store last IP between runs
+my $ipfile = '/home/username/.lastip';    # file to store last IP between runs
 
 # get public ip
-chomp (my $pubip = get('http://ip.thegrebs.com/'));
-chomp (my $oldip = `cat  $ipfile`);
+my $pubip = get('http://ip.thegrebs.com/') or exit 1;
+my $oldip = `cat  $ipfile`;
+
+for ($pubip, $oldip) { chomp if $_ }
 
 # exit if no change
 exit 0 if $oldip eq $pubip;
 
 # still running so update A record $record in $domain to point to current
 # public ip
-my $api = new WebService::Linode::DNS( apikey => $apikey );
+my $api = new WebService::Linode( apikey => $apikey );
 
-my $resourceid =
-	$api->getResourceIDbyName(domain => $domain, name => $record);
-die "Couldn't find RR id" unless $resourceid;
+my $domainid = $api->getDomainIDbyName($domain);
+die "Couldn't find Domain ID for $domain\n" unless $domainid;
 
-$api->domainResourceUpdate(resourceid => $resourceid, target => $pubip);
+my $resourceid = $api->getDomainResourceIDbyName(domainid => $domainid, name => $record);
+die "Couldn't find RR id for $record\n" unless $resourceid;
+
+my $result = $api->domain_resource_update(domainid=> $domainid, resourceid => $resourceid, target => $pubip);
+die "Error updating RR :<" unless $result->{resourceid} == $resourceid;
 
 system "echo '$pubip' > $ipfile";
